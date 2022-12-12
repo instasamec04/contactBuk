e8adc1ac9264f9ac59141a3f1a63807d4958a922
unknown <berdibekovsamatxann@gmail.com>
2022-12-12 18:01:27 +0600

    first commit

---
+++ src/components/EditContacts/EditContacts.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const EditContacts = (props) => {
+  let [name, setName] = useState(props.editContact.name);
+  let [surname, setSurname] = useState(props.editContact.surname);
+  let [phone, setPhone] = useState(props.editContact.phone);
+
+  function handleSaveClick() {
+    let contact = { ...props.editContact };
+    contact.name = name;
+    contact.surname = surname;
+    contact.phone = phone;
+    props.handleSaveEditContact(contact);
+    setName("");
+    setSurname("");
+    setPhone("");
+  }
+  return (
+    <div>
+      <input
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        placeholder="Name"
+        value={name}
+      />
+      <input
+        onChange={(e) => setSurname(e.target.value)}
+        type="text"
+        placeholder="Surname"
+        value={surname}
+      />
+      <input
+        onChange={(e) => setPhone(e.target.value)}
+        type="text"
+        placeholder="Number"
+        value={phone}
+      />
+      <button onClick={handleSaveClick}>Save</button>
+    </div>
+  );
+};
+
+export default EditContacts;
